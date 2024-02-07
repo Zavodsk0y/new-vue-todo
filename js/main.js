f339f283f01ec9739cc914dd92c3fed9e3e6095a
Zavodsk0y <zimniyvolk5@gmail.com>
2024-02-07 13:31:20 +0700

    a teper' cherez new vue

--- js/main.js
+++ js/main.js
@@ -1,49 +1,58 @@
-Vue.component('columns', {
-    props: ['cards'],
-    template: `
-    <div class="columns">
-        <div class="column" id="important-column">
-            <h2 class="t-a-c">Бэклог</h2>
-            <input type="text" v-model="name" placeholder="Название карточки">
-            <button @click="addCard(name)">+</button>
-            <ul>
-                <li v-for="(card, index) in cards">{{card.name}} {{card.tasks}}</li>
-                <input type="text" v-model="taskName" placeholder="Добавьте задачу">
-                <button @click="addTaskToCard(card)">+</button>
-            </ul>
-        </div>
-        <div class="column" id="important-column">
-            <h2 class="t-a-c">В работе</h2>
-        </div>
-        <div class="column" id="important-column">
-            <h2 class="t-a-c">Готово</h2>
-        </div>
-    </div>
-    `,
-    methods: {
-        addCard(name) {
-            let card = {name: name, tasks: []}
-            this.cards.push(card)
-            this.name = ""
-            console.log(this.cards)
-        },
-        addTaskToCard(card) {
-            console.log(Object.values(this.cards[0].tasks))
-        }
-    },
-    data() {
-        return {
-            name: null,
-            taskName: null,
-        }
-    }
-})
+// Vue.component('columns', {
+//     props: ['cards'],
+//     template: `
+//
+//     `,
+//     methods: {
+//         addCard(name) {
+//             let card = {name: name, tasks: []}
+//             this.cards.push(card)
+//             this.name = ""
+//             console.log(this.cards)
+//         },
+//         addTaskToCard(card) {
+//             console.log(Object.values(this.cards[0].tasks))
+//         }
+//     },
+//     data() {
+//         return {
+//             name: null,
+//             taskName: null,
+//         }
+//     }
+// })
 
 
 new Vue({
     el: "#todo-container",
     data: {
-        cards: [
-        ]
+        firstColumnCards: [],
+        secondColumnCards: [],
+        thirdColumnCards: [],
+        cardName: '',
+        firstColumnQuantity: 0,
+        secondColumnQuantity: 0,
+        taskName: ''
+    },
+    methods: {
+        addCard() {
+            if (this.firstColumnQuantity < 3) {
+                let card = {name: this.cardName, tasks: []}
+                this.firstColumnCards.push(card)
+                this.firstColumnQuantity += 1
+                console.log(this.firstColumnCards)
+            }
+        },
+        addTask(cardIndex) {
+            console.log(cardIndex)
+            let task = {taskName: this.taskName, completed: false}
+            this.firstColumnCards[cardIndex].tasks.push(task)
+            console.log(this.firstColumnCards[cardIndex].tasks)
+        },
+        completeTask(card, taskIndex) {
+            card.tasks[taskIndex].completed = !card.tasks[taskIndex].completed
+        }
+
+
     }
 })
