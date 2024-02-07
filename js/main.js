32116c112f5b9223be0b95aa1b018bc5228fa65f
Zavodsk0y <zimniyvolk5@gmail.com>
2024-02-07 14:46:04 +0700

    найс оно работает (вроде)

--- js/main.js
+++ js/main.js
@@ -15,31 +15,27 @@ new Vue({
                 let card = {name: this.cardName, tasks: [], taskName: ''}
                 this.firstColumnCards.push(card)
                 this.firstColumnQuantity += 1
-                console.log(this.firstColumnCards)
             }
         },
         addTask(cardIndex) {
-            console.log(cardIndex)
             let task = {taskName: this.firstColumnCards[cardIndex].taskName, completed: false}
             this.firstColumnCards[cardIndex].tasks.push(task)
             this.firstColumnCards[cardIndex].taskName = ''
-            console.log(this.firstColumnCards[cardIndex].tasks)
         },
         completeTask(card, taskIndex, cardIndex) {
-            console.log(card)
-            console.log(cardIndex)
             card.tasks[taskIndex].completed = !card.tasks[taskIndex].completed
             if (this.isCardHalfCompleted(card)) this.moveCardToInProgress(cardIndex)
             if (this.isCardCompleted(card)) {
-                alert('pizda')
                 this.moveCardToDone(cardIndex)
             }
         },
         moveCardToInProgress(cardIndex) {
+            console.log(cardIndex)
             this.secondColumnCards.push(this.firstColumnCards[cardIndex])
             this.firstColumnCards.splice(cardIndex, 1)
         },
         moveCardToDone(cardIndex) {
+            console.log(cardIndex)
             this.thirdColumnCards.push(this.secondColumnCards[cardIndex])
             this.secondColumnCards.splice(cardIndex, 1)
         },
